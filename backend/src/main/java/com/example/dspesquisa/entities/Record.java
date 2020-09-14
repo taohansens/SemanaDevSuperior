4852a49bdc9a50844d5f36ac5242237f9768d798
Tao Hansen <taohansens@gmail.c>
2020-09-14 20:45:45 -0300

    entidades criadas

---
+++ backend/src/main/java/com/example/dspesquisa/entities/Record.java
@@ -0,0 +1,13 @@
+package com.example.dspesquisa.entities;
+
+import java.time.Instant;
+
+public class Record {
+	
+	private Long id;
+	private String name;
+	private Integer age;
+	private Instant moment;
+	
+	private Game game;
+}
---
+++ backend/src/main/java/com/example/dspesquisa/entities/enums/Platform.java
@@ -0,0 +1,5 @@
+package com.example.dspesquisa.entities.enums;
+
+public enum Platform {
+	PC, PLAYSTATION, XBOX;
+}

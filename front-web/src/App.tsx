148cabc5d4b37d0e66bb2fe94c7fd07c212a6ad2
Tao Hansen <taohansens@gmail.c>
2020-09-16 23:50:08 -0300

    clean code

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import "./App.css";
-import Hello from "./Hello";
 
 function App() {
   return (
     <div>
-      <Hello message='semana devsuperior'/>
-      <Hello message='semana devsuperior'/>
-      <Hello message='semana devsuperior'/>
-      <Hello message='semana devsuperior'/>
     </div>
   );
 }

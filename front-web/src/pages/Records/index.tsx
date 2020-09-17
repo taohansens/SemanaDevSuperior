bfd3a2d6fce8aac5f8fc517a0577723c6fae0235
Tao Hansen <taohansens@gmail.c>
2020-09-17 20:05:43 -0300

    axios installed, table of records ok

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import "./styles.css";
 
-const Records = () => (
-    <h1>Records</h1>
+const Records = () => {
+
+    useEffect(() => {
+        console.log("Componente inciado");
+    },[]);
+
+
+    return (
+    <div className="page-container">
+        <table className="records-table" cellPadding="0" cellSpacing="0">
+            <thead>
+                <tr>
+                    <th>INSTANTE</th>
+                    <th>NOME</th>
+                    <th>IDADE</th>
+                    <th>PLATAFORMA</th>
+                    <th>GÊNERO</th>
+                    <th>TÍTULO DO GAME</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>17/09/2020 19:45</td>
+                    <td>TAO HANSEN</td>
+                    <td>30</td>
+                    <td>PC</td>
+                    <td>BATTLE ROYALE</td>
+                    <td>FORTNITE</td>
+                </tr>
+                <tr>
+                    <td>17/09/2020 19:45</td>
+                    <td>TAO HANSEN</td>
+                    <td>30</td>
+                    <td>PC</td>
+                    <td>BATTLE ROYALE</td>
+                    <td>FORTNITE</td>
+                </tr>
+                <tr>
+                    <td>17/09/2020 19:45</td>
+                    <td>TAO HANSEN</td>
+                    <td>30</td>
+                    <td>PC</td>
+                    <td>BATTLE ROYALE</td>
+                    <td>FORTNITE</td>
+                </tr>
+                <tr>
+                    <td>17/09/2020 19:45</td>
+                    <td>TAO HANSEN</td>
+                    <td>30</td>
+                    <td>PC</td>
+                    <td>BATTLE ROYALE</td>
+                    <td>FORTNITE</td>
+                </tr>
+            </tbody>
+
+        </table>
+    </div>
 );
+    }
 
 export default Records;

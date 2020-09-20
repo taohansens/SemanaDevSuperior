4870a3bb585fbb914aa74b29f215ebaf162d104a
Tao Hansen <taohansens@gmail.c>
2020-09-19 23:28:52 -0300

    heroku database integrated

--- front-mobile/src/Pages/CreateRecord/index.tsx
+++ front-mobile/src/Pages/CreateRecord/index.tsx
@@ -13,7 +13,7 @@ const placeholder = {
     value: null
 }
 
-const BASE_URL = 'http://10.0.0.3:8080'
+const BASE_URL = 'https://sdevsuperior1-tao.herokuapp.com';
 
 const mapSelectValues = (games: Game[]) => {
     return games.map(game => ({

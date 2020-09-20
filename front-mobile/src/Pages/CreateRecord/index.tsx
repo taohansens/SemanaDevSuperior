3a387633fb8db367a65d14a3e77a94c5cc70d22a
Tao Hansen <taohansens@gmail.c>
2020-09-19 23:22:56 -0300

    integracao com API ok

--- front-mobile/src/Pages/CreateRecord/index.tsx
+++ front-mobile/src/Pages/CreateRecord/index.tsx
@@ -1,25 +1,69 @@
-import React, { useState } from "react";
-import { StyleSheet, View, TextInput } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, StyleSheet, View, TextInput, Alert } from "react-native";
 import { FontAwesome5 as Icon } from '@expo/vector-icons'
 import Header from "../../components/Header";
 import PlatformCard from "./PlatformCard";
-import { GamePlatform } from "./types";
+import { GamePlatform, Game } from "./types";
 import RNPickerSelect from 'react-native-picker-select';
+import axios from 'axios';
+import { RectButton } from "react-native-gesture-handler";
 
 const placeholder = {
     label: 'Selecione o game',
     value: null
 }
 
+const BASE_URL = 'http://10.0.0.3:8080'
+
+const mapSelectValues = (games: Game[]) => {
+    return games.map(game => ({
+        ...game,
+        label: game.title,
+        value: game.id
+    }));
+}
+
 const CreateRecord = () => {
-  const [platform, setPlatform] = useState<GamePlatform>();
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+    const [platform, setPlatform] = useState<GamePlatform>();
+    const [selectedGame, setSelectedGame] = useState('');
+    const [allGames, setAllGames] = useState<Game[]>([]);
+    const [filteredGames, setFilteredGames] = useState<Game[]>([]);
+
+    const handChangePlatform = (selectedPlatform: GamePlatform) => {
+        setPlatform(selectedPlatform);
+        const gamesByPlatform = allGames.filter(
+            game => game.platform === selectedPlatform
+        )
+        setFilteredGames(gamesByPlatform);
+    };
+
+    const handleSubmit = () => {
+        const payload = {name, age, gameId: selectedGame};
+
+        axios.post(`${BASE_URL}/records`, payload)
+            .then(() => {
+                Alert.alert('Dados salvos com sucesso!');
+                setName('');
+                setAge('');
+                setSelectedGame('');
+                setPlatform(undefined);
+            })
+            .catch(()=> Alert.alert('Erro ao salvar os dados.'))
+    }
+
+
+    useEffect (() => {
+        axios.get(`${BASE_URL}/games`)
+        .then(response => {
+            const selectValues = mapSelectValues(response.data);
+
+            setAllGames(selectValues);
+        })
+        .catch(()=> Alert.alert('Erro ao listar os jogos.'))
+    }, []);
   
-  const [selectedGame, setSelectedGame] = useState('');
-
-  const handChangePlatform = (selectedPlatform: GamePlatform) => {
-    setPlatform(selectedPlatform);
-  };
-
   return (
     <>
       <Header />
@@ -28,6 +72,8 @@ const CreateRecord = () => {
           style={styles.inputText}
           placeholder="Nome"
           placeholderTextColor="#9E9E9E"
+          onChangeText={text => setName(text)}
+          value={name}
         />
         <TextInput
           keyboardType="numeric"
@@ -35,6 +81,8 @@ const CreateRecord = () => {
           style={styles.inputText}
           placeholder="Idade"
           placeholderTextColor="#9E9E9E"
+          onChangeText={text => setAge(text)}
+          value={age}
         />
 
         <View style={styles.platformContainer}>
@@ -61,17 +109,21 @@ const CreateRecord = () => {
             onValueChange={value => {
                 setSelectedGame(value);}}
             placeholder={placeholder}
-            items={[
-                {label: 'Football', value: 'football'},
-                {label: 'Baseball', value: 'baseball'},
-                {label: 'Hockey', value: 'hockey'},
-                ]
-            }
+            value={selectedGame}
+            items={filteredGames}
             style={pickerSelectedStyles}
             Icon={() => {
                 return <Icon name="chevron-down" color="#9E9E9E" size={25} />
             }}
             />
+      </View>
+      <View style={styles.footer}>
+          <RectButton style={styles.button} onPress={handleSubmit}>
+              <Text style={styles.buttonText}>
+                  SALVAR
+              </Text>
+          </RectButton>
+
       </View>
     </>
   );
